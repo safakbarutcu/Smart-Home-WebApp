958c9998ca3e56ccf3b9d815dd468ae26c92f98d
safakbarutcu <barutcusafak@gmail.com>
2023-04-23 19:13:19 +0300

    update design

--- login.php
+++ login.php
@@ -4,7 +4,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $_POST["username"];
   $password = $_POST["password"];
 
-  //use asd for username and password for consumer login.
   if ($username === "c" && $password === "123") {
     header("Location: consumer_home.html");
     exit();
